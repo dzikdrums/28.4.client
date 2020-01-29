57b910ca2f06a664e1abf2f25e3d2d5b43ae9967
Maciej Dzik <dzikdrums@gmail.com>
2020-01-29 19:57:35 +0100

    client

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    tasks: ['buty', 'zakupy'],
+    taskName: ''
+  };
+
+  
+  socket = io({
+    autoConnect: false
+  });
+
+  componentDidMount() {
+    this.socket.connect("localhost:8000");
+    // this.socket.on('addTask', (task) => this.addTask(task));
+    // this.socket.on('removeTask', (task) => this.removeTask(task));
+
+  };
+
+  handleNameChange = (event) => {
+    this.setState({ taskName: event.target.value });
+    console.log(this.state.taskName);
+  };
+
+  updateTasks = (arrayOfTasks) => {
+    this.setState({tasks: arrayOfTasks})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.addTask(this.state.taskName);
+    // this.socket.emit('addTask', this.state.taskName);
+  };
+
+  addTask(task) {
+    const updatedArray = [...this.state.tasks];
+    updatedArray.push(task);
+    this.setState({tasks: updatedArray});
+  };
+
+  removeTask = () => {
+    console.log('remove');
+    // const updatedArray = [...this.state.tasks];
+
+    // function isEqualTo(element) {
+    //   return element === id
+    // }
+    // const indexOf = updatedArray.findIndex(isEqualTo)
+
+    // updatedArray.splice(indexOf, 1);
+    // this.setState({tasks: updatedArray});
+    // this.socket.emit('removeTask', {id: id});
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>ToDoList.app</h1> 
+        </header>
+
+        <section className="tasks-section">
+          <h2>The list</h2>
+
+          <ul className="tasks-section__list">
+            {this.state.tasks.map(function(task) {
+              return (
+                  <li key={task} className="task">{task} 
+                  <button className="btn btn--red" id="btn">Remove</button></li>
+                );
+            })}
+          </ul>
+
+          <form className="add-task-form" onSubmit={this.handleSubmit}>
+            <input 
+              className="taskInput" 
+              autoComplete="off" 
+              type="text" 
+              placeholder="type your task" 
+              value={this.state.taskName}
+              onChange={this.handleNameChange}
+            />
+            <button className="btn" type="submit">Add</button>
+          </form>
+        </section>
+      </div>
+    );
+  };
 }
 
 export default App;
